705d49d02691e833785681af246bc60e0d05e648
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2014-10-22 14:20:43 +0000

    Fix test suite compatibility with older Rubies.
    The ability to chain method calls with the dot on a new line was
    introduced in Ruby 1.9.1 (the changelog is available at
    <http://svn.ruby-lang.org/repos/ruby/tags/v1_9_1_0/NEWS>). Since we're
    still maintaining compatibility with 1.8.x versions (at least for now),
    we have to stick with the less pretty syntax.

--- test/rubygems_plugin_test.rb
+++ test/rubygems_plugin_test.rb
@@ -10,9 +10,9 @@ module GemPublisher
     end
 
     def test_should_report_published_gem
-      GemPublisher.expects(:publish_if_updated)
-                  .with("awesome.gemspec", "rubygems")
-                  .returns("awesome-1.0.0.gem")
+      GemPublisher.expects(:publish_if_updated).
+                   with("awesome.gemspec", "rubygems").
+                   returns("awesome-1.0.0.gem")
       $stderr.expects(:puts).with("Published awesome-1.0.0.gem")
 
       @command.handle_options ["awesome.gemspec"]
@@ -20,9 +20,9 @@ module GemPublisher
     end
 
     def test_should_not_report_unpublished_gem
-      GemPublisher.expects(:publish_if_updated)
-                  .with("awesome.gemspec", "rubygems")
-                  .returns(nil)
+      GemPublisher.expects(:publish_if_updated).
+                   with("awesome.gemspec", "rubygems").
+                   returns(nil)
       $stderr.expects(:puts).with("Nothing to do for awesome.gemspec")
 
       @command.handle_options ["awesome.gemspec"]
@@ -30,9 +30,9 @@ module GemPublisher
     end
 
     def test_should_use_gemfury_if_requested
-      GemPublisher.expects(:publish_if_updated)
-                  .with("awesome.gemspec", "gemfury")
-                  .returns("awesome-1.0.0.gem")
+      GemPublisher.expects(:publish_if_updated).
+                   with("awesome.gemspec", "gemfury").
+                   returns("awesome-1.0.0.gem")
       $stderr.expects(:puts).with(anything)
 
       @command.handle_options ["-r", "gemfury", "awesome.gemspec"]
@@ -40,9 +40,9 @@ module GemPublisher
     end
 
     def test_should_use_gemfury_with_long_option
-      GemPublisher.expects(:publish_if_updated)
-                  .with("awesome.gemspec", "gemfury")
-                  .returns("awesome-1.0.0.gem")
+      GemPublisher.expects(:publish_if_updated).
+                   with("awesome.gemspec", "gemfury").
+                   returns("awesome-1.0.0.gem")
       $stderr.expects(:puts).with(anything)
 
       @command.handle_options ["--repository=gemfury", "awesome.gemspec"]
